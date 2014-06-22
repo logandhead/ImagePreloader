855142f3fc92c7e947acbfcb1e7a69750906f4aa
logancanvas <lhead@canvas.is>
2014-06-22 10:55:44 -0400

    first commit

---
+++ src/ImgPreloader.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var imgPreloader = (function () {
+        var load = function (opts) {
+            var options = $.extend({
+                src: '',
+                html: '',
+                callback: null,
+                width: 300,
+                height: 300,
+            }, opts);
+            var image = new Image(300, 300);
+            image.src = src;
+            image.onload = function () {
+                if (options.html  != '' && options.callback != null)
+                    options.callback(options.html);
+            };
+        }
+        return {
+            load: load
+        };
+    }());
+});
